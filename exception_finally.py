75b2edaae9174072c2c7fc9c04b598353e36b247
kuailexiaoxiao <535762828@qq.com>
2017-08-14 11:23:14 +0800

    继承练习

---
+++ exception_finally.py
@@ -0,0 +1,34 @@
+import sys
+import time
+
+poem = '''\
+Programming is fun
+When the work is done
+if you wanna make your work also fun:
+use Python!
+'''
+f = open('poem.txt')
+line = f.readline()
+if len(line) == 0:
+    f = open('poem.txt', 'w')
+    f.write(poem)
+    f.close()
+
+try:
+    f = open('poem.txt')
+    while True:
+        line = f.readline()
+        if len(line) == 0:
+            break
+        print(line, end=" ")
+        sys.stdout.flush()
+        print("Press ctrl+c now")
+        time.sleep(2)
+except IOError:
+    print("Could not find file poem.txt")
+except KeyboardInterrupt:
+    print("!! You cancelled the reading from the file.")
+finally:
+    if f:
+        f.close()
+    print("Cleaning up : closed the file")
